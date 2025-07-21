0644186981a97d14697b3df23cc22f7d3764800b
Viswanathan P <drummerviswa@gmail.com>
2025-07-21 12:51:02 +0530

    Feat: 1.0.5

--- src/preload/index.js
+++ src/preload/index.js
@@ -3,5 +3,11 @@ import { contextBridge, ipcRenderer } from 'electron'
 contextBridge.exposeInMainWorld('api', {
   updateMessage: (callback) => {
     ipcRenderer.on('update_available', callback)
+  },
+  checkForUpdates: () => {
+    ipcRenderer.send('check_for_updates')
+  },
+  downloadUpdate: () => {
+    ipcRenderer.send('download_update')
   }
 })
